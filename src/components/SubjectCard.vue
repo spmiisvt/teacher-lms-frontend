3d8dd38fc5c69c0eb3b74f8db9aa7f116b8ef337
weblab <spmi.isvt@gmail.com>
2022-06-22 13:28:18 +0300

    tips

--- src/components/SubjectCard.vue
+++ src/components/SubjectCard.vue
@@ -3,6 +3,21 @@
      <h3 class="subject__cart--title">
        ЭВМ и Переферийные устройства
      </h3>
+    <div class="control__items">
+      <div>
+        <i class='bx bx-sync bx-sm'></i>
+      </div>
+      <div>
+        <i class='bx bx-plus-circle bx-sm'></i>
+      </div>
+     <div>
+       <i class='bx bx-minus-circle bx-sm'></i>
+     </div>
+    </div>
+
+
+
+
   </div>
 </template>
 
@@ -14,6 +29,8 @@ export default {
 
 <style lang="scss">
 .subject__card {
+  display: flex;
+  justify-content: space-evenly;
   background-color: white;
   width: 250px;
   height: 125px;
@@ -25,4 +42,31 @@ export default {
     font-weight: 400;
   }
 }
+.control__items {
+  position: relative;
+  display: block;
+  div {
+    border: 1px solid var(--fg-primary);
+    margin: 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 5px;
+    width: 27px;
+    height: 27px;
+    cursor: pointer;
+    transition: .3s ease;
+    i {
+      color: var(--hi-primary);
+      transition: .3s ease;
+    }
+    &:hover {
+      background: var(--hi-primary);
+      i {
+        color: var(--fg-primary);
+      }
+    }
+  }
+}
+
 </style>
